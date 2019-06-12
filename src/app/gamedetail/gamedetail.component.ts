861d9d8f581135dfe4747f9cf92a053ba9511049
Abhishek Sarolia <sarolia4u@gmail.com>
2019-06-12 18:48:14 +0530

    Added support for Http service as observable and loading spinner

---
+++ src/app/gamedetail/gamedetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamedetail',
+  templateUrl: './gamedetail.component.html',
+  styleUrls: ['./gamedetail.component.css']
+})
+export class GamedetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
